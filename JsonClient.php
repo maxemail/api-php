4e74e50730d492fbfd1dc62f046aa19bfbd6bd27
Chris Minett <chris@minett.me.uk>
2014-10-16 15:45:19 +0100

    Alter main class name to be more generic to the API. Clean up formatting.

--- JsonClient.php
+++ JsonClient.php
@@ -3,21 +3,27 @@
 /**
  * MXM JSON API Client
  *
- * v1.4, config array version
+ * v1.5, config array version
  *
  * @category   Mxm
  * @package    Mxm_Api
  * @copyright  Copyright (c) 2007-2012 Emailcenter UK. (http://www.emailcenteruk.com)
  * @license    Commercial
  */
-class Mxm_Api_JsonClient
+class Mxm_Api
 {
- 
+    /** @var array */
     protected $services = array();
-    protected $url      = null;
+
+    /** @var string */
+    protected $url = null;
+
+    /** @var string */
     protected $username = null;
+
+    /** @var string */
     protected $password = null;
- 
+
     /**
      * Construct
      *
@@ -29,49 +35,49 @@ class Mxm_Api_JsonClient
         $this->username = $config['user'];
         $this->password = $config['pass'];
     }
- 
+
     /**
      * Get JsonClient for selected service
      *
      * @param string $service
-     * @return JsonClient
+     * @return Mxm_Api_JsonClient
      */
     public function getInstance($service)
     {
         if (!isset($this->services[$service])) {
             $url = $this->url . $service;
-            $this->services[$service] = new JsonClient($url, $this->username, $this->password);
+            $this->services[$service] = new Mxm_Api_JsonClient($url, $this->username, $this->password);
         }
         return $this->services[$service];
     }
- 
+
     /**
      * Magic get for service
      *
      * @param string $name
-     * @return JsonClient
+     * @return Mxm_Api_JsonClient
      */
     public function __get($name)
     {
         return $this->getInstance($name);
     }
- 
 }
- 
+
 /**
  * MXM JSON API Client
  *
- * v1.4, config array version
+ * v1.5, config array version
  *
  * @category   Mxm
  * @package    Mxm_Api
  * @copyright  Copyright (c) 2007-2012 Emailcenter UK. (http://www.emailcenteruk.com)
  * @license    Commercial
  */
-class JsonClient
+class Mxm_Api_JsonClient
 {
- 
+
     /**
+     * Construct
      *
      * @param string $url
      * @param string $username
@@ -80,7 +86,7 @@ class JsonClient
     public function __construct($url, $username, $password)
     {
         $this->url = $url;
- 
+
         $parts = parse_url($url);
         $this->scheme   = $parts['scheme'];
         $this->host     = $parts['host'];
@@ -88,76 +94,79 @@ class JsonClient
         $this->username = $username;
         $this->password = $password;
     }
- 
+
     /**
      * Post request
      *
      * @param array $data
      * @return string
      */
-    protected function _postRequest(array $data)
+    protected function postRequest(array $data)
     {
         $port = strtolower($this->scheme) == 'https' ? 443 : 80;
         $host = strtolower($this->scheme) == 'https' ? 'ssl://' : '';
         $host.= $this->host;
- 
+
         $socket = @fsockopen($host, $port);
         if ($socket === false) {
             $error = error_get_last();
             throw new Exception("Failed to connect to {$this->host} on port $port, {$error['message']}");
         }
- 
-        $basicAuth = base64_encode($this->username . ':' . $this->password);
+
         $body = http_build_query($data);
- 
         $headers = array(
-            'Host'              => $this->host,
-            'Connection'        => 'close',
-            'Authorization'     => "Basic $basicAuth",
-            'Content-type'      => 'application/x-www-form-urlencoded',
-            'Content-length'    => strlen($body),
-            'User-Agent'        => 'MxmJsonClient/1.4a PHP/' . phpversion()
+            'Host'           => $this->host,
+            'Connection'     => 'close',
+            'Content-type'   => 'application/x-www-form-urlencoded',
+            'Content-length' => strlen($body),
+            'User-Agent'     => 'MxmJsonClient/1.5a PHP/' . phpversion()
         );
+
+        if (!is_null($this->username) && !is_null($this->password)) {
+            $basicAuth                = base64_encode($this->username . ':' . $this->password);
+            $headers['Authorization'] = "Basic $basicAuth";
+        }
+
         $request = "POST {$this->path} HTTP/1.0\r\n";
         foreach ($headers as $key => $value) {
             $request .= "$key: $value\r\n";
         }
         $request .= "\r\n$body";
         $this->lastRequest = $request;
- 
+
         if (@fwrite($socket, $request) === false) {
             $error = error_get_last();
             throw new Exception("Failed to write to socket, {$error['message']}");
         }
- 
+
         $response = '';
         while (!feof($socket)) {
             $response .= fread($socket, 8192);
         }
         @fclose($socket);
- 
+
         $this->lastResponse = $response;
- 
+
         preg_match("|^HTTP/[\d\.x]+ (\d+)|", $response, $matches);
         $code = (int)$matches[1];
- 
-        $parts = preg_split('|(?:\r?\n){2}|m', $response, 2);
+
+        $parts   = preg_split('|(?:\r?\n){2}|m', $response, 2);
         $content = $parts[1];
- 
+
         if ((int)$code != 200) {
             try {
-                $message = (array)$this->_decodeJson($content);
+                $message = (array)$this->decodeJson($content);
                 if (array_key_exists('msg', $message)) {
                     $content = $message['msg'];
                 }
             } catch (Exception $e) {
- 
+                // Void
             }
             throw new Exception($content, $code);
         }
         return $content;
     }
- 
+
     /**
      * Last request
      *
@@ -167,7 +176,7 @@ class JsonClient
     {
         return $this->lastRequest;
     }
- 
+
     /**
      * Last response
      *
@@ -177,16 +186,18 @@ class JsonClient
     {
         return $this->lastResponse;
     }
- 
+
     /**
      * Decode JSON
      *
      * @param string $json
-     * @return array
+     * @return mixed
      */
-    private function _decodeJson($json)
+    protected function decodeJson($json)
     {
         $result = json_decode($json, false);
+
+        // Error checking
         $ver = version_compare(PHP_VERSION, '5.3');
         if ($ver == '-1') {
             // Less than php5.3
@@ -212,30 +223,37 @@ class JsonClient
                     break;
             }
         }
- 
+
         if (!empty($error)) {
             throw new Exception("Problem decoding json ($json), {$error}");
         }
+
         return $result;
     }
- 
+
+    /**
+     * Magic call for service method
+     *
+     * @param string $name
+     * @param array $params
+     * @return mixed
+     */
     public function __call($name, $params)
     {
         $data = array(
             'method' => $name
         );
-        for ($i = 0; $i < count($params); $i++) {
-            if (is_array($params[$i])) {
-                $params[$i] = json_encode($params[$i]);
+        foreach ($params as $i => $param) {
+            if (is_array($param)) {
+                $param = json_encode($param);
             }
-            $data['arg' . $i] = $params[$i];
+            $data['arg' . $i] = $param;
         }
-        $json = $this->_postRequest($data);
+        $json = $this->postRequest($data);
         if ($json == 'null') {
             return null;
         }
-        return $this->_decodeJson($json);
-    }
- 
-}
 
+        return $this->decodeJson($json);
+    }
+}
