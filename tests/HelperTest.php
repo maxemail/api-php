30c35e6a69ebd59094b31e215956cd3173bdf422
Chris Minett <chris.minett@emailcenteruk.com>
2017-08-04 16:32:56 +0100

    Add tests for download helper

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace Emailcenter\MaxemailApi;
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware as GuzzleMiddleware;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use phpmock\phpunit\PHPMock;
 use PHPUnit\Framework\TestCase;
@@ -15,23 +19,26 @@ class HelperTest extends TestCase
     /**
      * @var Client|\PHPUnit_Framework_MockObject_MockObject
      */
-    private $client;
+    private $apiClientMock;
 
     /**
      * @var GuzzleClient|\PHPUnit_Framework_MockObject_MockObject
      */
-    private $httpClient;
+    private $httpClientMock;
 
     /**
-     * @var Helper
+     * @var MockHandler
      */
-    private $helper;
+    private $mockHandler;
+
+    private $clientHistory = [];
 
     protected function setUp()
     {
-        $this->client = $this->createMock(Client::class);
-        $this->httpClient = $this->createMock(GuzzleClient::class);
-        $this->helper = new Helper($this->client, $this->httpClient);
+        $this->apiClientMock = $this->createMock(Client::class);
+        $this->httpClientMock = $this->createMock(GuzzleClient::class);
+
+        $this->mockHandler = new MockHandler();
 
         // This allows fopen() to be overloaded after it's first used normally in other tests
         $this->defineFunctionMock(__NAMESPACE__, 'fopen');
@@ -46,7 +53,7 @@ class HelperTest extends TestCase
         $fileUploadService = $this->createMock(Service::class);
 
         // The API client will be used to initialise the upload
-        $this->client->expects($this->once())
+        $this->apiClientMock->expects($this->once())
             ->method('__get')
             ->with('file_upload')
             ->willReturn($fileUploadService);
@@ -86,12 +93,12 @@ class HelperTest extends TestCase
             return true;
         };
         $uploadResponse = new Response(200, [], '');
-        $this->httpClient->expects($this->once())
+        $this->httpClientMock->expects($this->once())
             ->method('request')
             ->with('POST', 'file_upload', $this->callback($checkMultipart))
             ->willReturn($uploadResponse);
 
-        $actual = $this->helper->uploadFile($sampleFile);
+        $actual = $this->getHelperWithMockClient()->uploadFile($sampleFile);
 
         $this->assertEquals($key, $actual);
     }
@@ -103,7 +110,7 @@ class HelperTest extends TestCase
 
         $sampleFile = __DIR__ . '/__files/does-not-exist';
 
-        $this->helper->uploadFile($sampleFile);
+        $this->getHelperWithMockClient()->uploadFile($sampleFile);
     }
 
     public function testUploadUnableToOpen()
@@ -118,6 +125,183 @@ class HelperTest extends TestCase
             ->with($sampleFile, 'r')
             ->willReturn(false);
 
-        $this->helper->uploadFile($sampleFile);
+        $this->getHelperWithMockClient()->uploadFile($sampleFile);
+    }
+
+    public function testDownloadCsv()
+    {
+        $sampleFile = __DIR__ . '/__files/sample-file.csv';
+        $key = 'abc123def456';
+
+        $this->mockHandler->append(
+            new Response(200, [], fopen($sampleFile, 'r'))
+        );
+
+        $downloadedFile = $this->getHelperWithMockHandler()->downloadFile('file', $key);
+
+        $this->assertCount(1, $this->clientHistory);
+
+        /** @var Request $request */
+        $request = $this->clientHistory[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('/download/file/key/' . $key, $request->getUri()->getPath());
+        $this->assertEquals(['*'], $request->getHeader('Accept'));
+
+        $this->assertFileEquals($sampleFile, $downloadedFile);
+    }
+
+    public function testDownloadZip()
+    {
+        $responseFile = __DIR__ . '/__files/sample-file.csv.zip';
+        $expectedFile = __DIR__ . '/__files/sample-file.csv';
+        $key = 'abc123def456';
+
+        $this->mockHandler->append(
+            new Response(200, [], fopen($responseFile, 'r'))
+        );
+
+        $downloadedFile = $this->getHelperWithMockHandler()->downloadFile('file', $key);
+
+        $this->assertCount(1, $this->clientHistory);
+
+        /** @var Request $request */
+        $request = $this->clientHistory[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('/download/file/key/' . $key, $request->getUri()->getPath());
+        $this->assertEquals(['*'], $request->getHeader('Accept'));
+
+        $this->assertFileEquals($expectedFile, $downloadedFile);
+    }
+
+    public function testDownloadZipNoExtract()
+    {
+        $sampleFile = __DIR__ . '/__files/sample-file.csv.zip';
+        $key = 'abc123def456';
+
+        $this->mockHandler->append(
+            new Response(200, [], fopen($sampleFile, 'r'))
+        );
+
+        $downloadedFile = $this->getHelperWithMockHandler()->downloadFile('file', $key, ['extract' => false]);
+
+        $this->assertCount(1, $this->clientHistory);
+
+        /** @var Request $request */
+        $request = $this->clientHistory[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('/download/file/key/' . $key, $request->getUri()->getPath());
+        $this->assertEquals(['*'], $request->getHeader('Accept'));
+
+        $this->assertFileEquals($sampleFile, $downloadedFile);
+    }
+
+    public function testDownloadPdf()
+    {
+        $sampleFile = __DIR__ . '/__files/sample-file.pdf';
+        $key = 'abc123def456';
+
+        $this->mockHandler->append(
+            new Response(200, [], fopen($sampleFile, 'r'))
+        );
+
+        $downloadedFilename = $this->getHelperWithMockHandler()->downloadFile('file', $key);
+
+        $this->assertCount(1, $this->clientHistory);
+
+        /** @var Request $request */
+        $request = $this->clientHistory[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('/download/file/key/' . $key, $request->getUri()->getPath());
+        $this->assertEquals(['*'], $request->getHeader('Accept'));
+
+        $this->assertFileEquals($sampleFile, $downloadedFilename);
+    }
+
+    public function testDownloadListExport()
+    {
+        $sampleFile = __DIR__ . '/__files/sample-file.csv';
+        $key = 123;
+
+        $this->mockHandler->append(
+            new Response(200, [], fopen($sampleFile, 'r'))
+        );
+
+        $downloadedFile = $this->getHelperWithMockHandler()->downloadFile('listexport', $key);
+
+        $this->assertCount(1, $this->clientHistory);
+
+        /** @var Request $request */
+        $request = $this->clientHistory[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('/download/listexport/id/' . $key, $request->getUri()->getPath());
+        $this->assertEquals(['*'], $request->getHeader('Accept'));
+
+        $this->assertFileEquals($sampleFile, $downloadedFile);
+    }
+
+    public function testDownloadDataExport()
+    {
+        $sampleFile = __DIR__ . '/__files/sample-file.csv';
+        $key = 123;
+
+        $this->mockHandler->append(
+            new Response(200, [], fopen($sampleFile, 'r'))
+        );
+
+        $downloadedFile = $this->getHelperWithMockHandler()->downloadFile('dataexport', $key);
+
+        $this->assertCount(1, $this->clientHistory);
+
+        /** @var Request $request */
+        $request = $this->clientHistory[0]['request'];
+
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('/download/dataexport/id/' . $key, $request->getUri()->getPath());
+        $this->assertEquals(['*'], $request->getHeader('Accept'));
+
+        $this->assertFileEquals($sampleFile, $downloadedFile);
+    }
+
+    public function testDownloadUnknownType()
+    {
+        $this->expectException(Exception\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid download type specified');
+
+        $this->getHelperWithMockHandler()->downloadFile('unknown', 123);
+    }
+
+    /**
+     * Get a Helper which uses a mock HTTP Client
+     *
+     * @return Helper
+     */
+    private function getHelperWithMockClient(): Helper
+    {
+        return new Helper($this->apiClientMock, $this->httpClientMock);
+    }
+
+    /**
+     * Get a Helper which uses an HTTP Client using the MockHandler and storing history
+     *
+     * @return Helper
+     */
+    private function getHelperWithMockHandler(): Helper
+    {
+        $stack = HandlerStack::create($this->mockHandler);
+        $stack->push(
+            GuzzleMiddleware::history($this->clientHistory)
+        );
+
+        $httpClient = new GuzzleClient([
+            'base_uri' => 'https://example.com/api/json/',
+            'handler' => $stack
+        ]);
+
+        return new Helper($this->apiClientMock, $httpClient);
     }
 }
